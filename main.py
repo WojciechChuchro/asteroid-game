8e2f2e9630ee5758263d1ed43cf729a9c16c89cb
Wojciech Chuchro <wojciech.chuchro@icloud.com>
2025-02-22 16:00:37 +0100

    Use groups for object managment

--- main.py
+++ main.py
@@ -2,27 +2,37 @@ import pygame
 from constants import *
 from player import Player
 
+
 def main():
-    print("Starting asteroids!")
-    print("Screen width:", SCREEN_WIDTH)
-    print("Screen height:", SCREEN_HEIGHT)
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
-    x = SCREEN_WIDTH/2
-    y = SCREEN_HEIGHT/2
-    player = Player(x,y)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
-        dt = clock.tick(60)/1000
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
